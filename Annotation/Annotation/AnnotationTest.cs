20ea72af7a40f97acf676dcd1fb27a17768fe2da
suraj9982 <surajsk9982@gmail.com>
2021-12-02 19:44:22 +0530

    [suraj] add : this is annotation demo

---
+++ Annotation/Annotation/AnnotationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation
+{
+    class AnnotationTest
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Employee class validation");
+            Console.WriteLine("-------------------------");
+            Employee objemployee = new Employee();
+            objemployee.Name = "suraj";
+            objemployee.Age = 22;
+            objemployee.PhoneNumber = "999999999";
+            objemployee.Email = "surajsk9982@gmail.com";
+            ValidationContext context = new ValidationContext(objemployee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(objemployee, context, results, true);
+            if (!valid)
+            {
+                foreach(ValidationResult totalresult in results)
+                {
+                    Console.Write("Member name : {0}", totalresult.MemberNames.First(), Environment.NewLine);
+                    Console.Write("Error Msg ::{0}{1}",totalresult.ErrorMessage,Environment.NewLine);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name: "+objemployee.Name + "\n" + "Age:"+objemployee.Age + "\n" + "Phonenumber:" + objemployee.PhoneNumber + "\n" + "EmailAddress:"+objemployee.Email);
+            }
+        }
+    }
+}
---
+++ Annotation/Annotation/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation
+{
+    class Employee
+    {
+        [Required(ErrorMessage ="Employe {0} is required")]
+        [StringLength(100,MinimumLength =3,ErrorMessage ="")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [Range(18,99,ErrorMessage ="age should be above 18")]
+        public int Age { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
